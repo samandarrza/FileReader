71ca138c0892f5f346628510ce7fe39a13f22546
samandarrza <samandarshr@code.edu.az>
2022-11-03 01:07:21 +0400

    add

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('file');
+
+input.addEventListener('change', function (e) {
+    console.log(input.files);
+    const reader = new FileReader();
+    reader.onload = function () {
+        const img = new Image();
+        img.src = reader.result;
+        document.body.appendChild(img);
+    }
+    reader.readAsDataURL(input.files[0]);
+},false)
